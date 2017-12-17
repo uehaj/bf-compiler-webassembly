559b10d8c5683d7ef63c63cc88971e378e54433e
@uehaj <uehaj@jggug.org>
2017-12-18 03:12:33 +0900

    commander works well

--- bin/index.js
+++ bin/index.js
@@ -3,44 +3,28 @@
 const program = require('commander');
 const driver = require('../lib/driver');
 
-function usage(program) {
-  program.help();
-  process.exit(-1);
-}
-
 program
   .version('0.1.0')
-  .option('-e,--script [string]', 'run script from command line')
-  .arguments('[files...]', 'Brainf*ck file')
+  .option('-e,--script [script]', 'run script from command line')
+  .arguments('[file...]', 'Brainf*ck file')
   .action(function(files, opts) {
     for (const file of files) {
-      driver.compileAndRunFile(file);
+      driver.compileAndRunFile(file).catch(e => console.log('error:' + e));
     }
+  })
+  .on('--help', function() {
+    console.log(`  Examples:'
+    $ npx bf-compiler-webassembly -e "+++"
+    $ npx bf-compiler-webassembly hello.bf"
+
+`);
   })
   .parse(process.argv);
 
-program.args.length !== 0 || usage(program);
-
-/*
-program
-  .version('0.1.0')
-  .option('-e,--script [string]', 'Brainf*ck script on command line')
-  .arguments('<bfFile> [bfFiles]', 'Brainf*ck file')
-  .parse(process.argv);
-
-if (program.args.length === 0) {
-  program.help();
-  process.exit(0);
-}
-console.log(program);
 if (program.script) {
-  console.log(program.script);
-  for (const target of program.args) {
-    console.log(target);
-    compileAndRunFile(target);
-  }
+  driver
+    .compileAndRunString(program.script)
+    .catch(e => console.log('error:' + e));
+} else {
+  program.args.length !== 0 || program.help();
 }
-if (program.script) {
-  compileAndRunString(target);
-}
-*/
